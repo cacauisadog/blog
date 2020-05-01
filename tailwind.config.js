a9262642738f2eef48d1ddaa41717c54f030d1fa
cacauisadog <cbcaue@gmail.com>
2020-04-30 21:49:59 -0300

    configure purgecss on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,15 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'blog/components/**/*.vue',
+      'blog/layouts/**/*.vue',
+      'blog/pages/**/*.vue',
+      'blog/plugins/**/*.js',
+      'blog/nuxt.config.js'
+    ]
+  }
 }
