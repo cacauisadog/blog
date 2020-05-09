73af32677480f9a3008260f00b00b3f3a46bae0d
cacauisadog <cbcaue@gmail.com>
2020-05-09 16:24:05 -0300

    add a navbar header

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="w-screen bg-black fixed top-0">
+    <ul class="h-12 container max-w-screen-md mx-auto px-4 md:px-0 flex text-lg text-gray-500 items-center">
+      <li class="cursor-pointer hover:text-gray-500">
+        <a>logo</a>
+      </li>
+      <li class="ml-auto cursor-pointer hover:text-white">
+        <a>blog</a>
+      </li>
+      <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
+        <a>sobre</a>
+      </li>
+      <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
+        <a>/uses</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <nuxt />
+    <Navbar />
+    <main class="mt-16">
+      <nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
