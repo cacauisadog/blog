7f99eb6825565e31717b76f8eaf775479a01d5d5
cacauisadog <cbcaue@gmail.com>
2020-05-26 23:37:36 -0300

    add nuxt-links to Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,16 +2,24 @@
   <header class="w-screen bg-black fixed top-0">
     <ul class="h-12 container max-w-screen-md mx-auto px-4 md:px-0 flex text-lg text-gray-500 items-center">
       <li class="cursor-pointer hover:text-gray-500">
-        <a>logo</a>
+        <nuxt-link :to="`/`">
+          logo
+        </nuxt-link>
       </li>
       <li class="ml-auto cursor-pointer hover:text-white">
-        <a>blog</a>
+        <nuxt-link :to="`/blog`">
+          blog
+        </nuxt-link>
       </li>
       <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
-        <a>sobre</a>
+        <nuxt-link :to="`/sobre`">
+          sobre
+        </nuxt-link>
       </li>
       <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
-        <a>/uses</a>
+        <nuxt-link :to="`/uses`">
+          /uses
+        </nuxt-link>
       </li>
     </ul>
   </header>
