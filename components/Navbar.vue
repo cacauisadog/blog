e935feef069ad9ec61cf0407a6034454808fc6ae
cacauisadog <cbcaue@gmail.com>
2020-05-29 00:40:11 -0300

    add padding for more clickability

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,22 +2,34 @@
   <header class="w-screen bg-black fixed top-0">
     <ul class="h-12 container max-w-screen-md mx-auto px-4 md:px-0 flex text-lg text-gray-500 items-center">
       <li class="cursor-pointer hover:text-gray-500">
-        <nuxt-link :to="`/`">
+        <nuxt-link
+          :to="`/`"
+          class="p-2"
+        >
           logo
         </nuxt-link>
       </li>
       <li class="ml-auto cursor-pointer hover:text-white">
-        <nuxt-link :to="`/blog`">
+        <nuxt-link
+          :to="`/blog`"
+          class="p-2"
+        >
           blog
         </nuxt-link>
       </li>
-      <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
-        <nuxt-link :to="`/sobre`">
+      <li class="ml-2 md:ml-4 cursor-pointer hover:text-white">
+        <nuxt-link
+          :to="`/sobre`"
+          class="p-2"
+        >
           sobre
         </nuxt-link>
       </li>
-      <li class="ml-6 md:ml-8 cursor-pointer hover:text-white">
-        <nuxt-link :to="`/uses`">
+      <li class="ml-2 md:ml-4 cursor-pointer hover:text-white">
+        <nuxt-link
+          :to="`/uses`"
+          class="p-2"
+        >
           /uses
         </nuxt-link>
       </li>
