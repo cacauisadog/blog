bb0db64bb3ed7faeba3490f371d5f2535e7b98cb
cacauisadog <cbcaue@gmail.com>
2020-05-29 00:42:45 -0300

    add css and async component loading

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <h1 class="text-white">
-      blog index
+  <div class="container max-w-screen-md mx-auto px-4 md:px-0">
+    <h1 class="title">
+      List of blog posts
     </h1>
-    <template v-if="articles.length > 0">
-      <article-list
-        v-for="article in articles"
-        :key="article.slug"
-        :article="article"
-      />
-    </template>
+    <article-list
+      v-for="article in articles"
+      :key="article.slug"
+      :article="article"
+    />
   </div>
 </template>
 
 <script>
-import ArticleList from './components/-article.vue'
-
 export default {
-  components: { ArticleList },
+  components: {
+    ArticleList: () => import('./components/ArticleList.vue')
+  },
   async asyncData ({ $content }) {
     const articles = await $content('articles')
       .only(['title', 'description', 'slug'])
