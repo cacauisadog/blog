a8bc89650b3be1d08c71e4f6171c143bd513a047
cacauisadog <cbcaue@gmail.com>
2020-05-31 11:49:38 -0300

    show article date, better html structure

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -2,19 +2,19 @@
   <article class="container max-w-screen-md mx-auto md:p-0 p-4">
     <header>
       <h1 class="title">
-        {{ doc.title }}
+        {{ article.title }}
       </h1>
     </header>
 
-    <p class="text-xl mb-2">
-      {{ doc.description }}
+    <p class="description">
+      {{ article.description }}
     </p>
-    <p class="text-sm font-light">
-      {{ doc.date }}
+    <p class="date">
+      {{ article.date }}
     </p>
     <nuxt-content
       class="mt-8"
-      :document="doc"
+      :document="article"
     />
   </article>
 </template>
@@ -22,61 +22,9 @@
 <script>
 export default {
   async asyncData ({ $content, params }) {
-    const doc = await $content('articles', params.slug).fetch()
+    const article = await $content('articles', params.slug).fetch()
 
-    return { doc }
+    return { article }
   }
 }
 </script>
-
-<style lang="postcss">
-.nuxt-content > h2 {
-  font-weight: 400;
-  @apply text-3xl leading-tight mb-5 text-gray-100;
-}
-
-.nuxt-content > h3 {
-  font-weight: 400;
-  @apply text-2xl leading-tight mb-4 text-gray-100;
-}
-
-.nuxt-content > ul {
-  @apply ml-5 list-disc font-light text-xl leading-normal mb-12 text-gray-100;
-}
-
-.nuxt-content > ul > li {
-  @apply mb-4;
-}
-
-.nuxt-content > p {
-  @apply font-light text-xl leading-normal mb-12 text-gray-100;
-}
-
-.nuxt-content > p > a:hover {
-  @apply underline;
-}
-
-.nuxt-content > p > a:link {
-  @apply text-blue-600;
-}
-
-.nuxt-content > pre {
-  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
-  padding: 2.4rem;
-  border-radius: 4px;
-  overflow-x: scroll;
-  display: block;
-  margin-bottom: 3rem;
-  @apply bg-gray-800;
-}
-
-.nuxt-content > pre > code {
-  @apply bg-gray-800;
-}
-
-@screen sm {
-  .nuxt-content > code {
-    @apply text-base;
-  }
-}
-</style>
--- pages/blog/components/ArticleList.vue
+++ pages/blog/components/ArticleList.vue
@@ -4,22 +4,32 @@
       :to="`/blog/${article.slug}`"
       class="block"
     >
-      <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-200 dark:text-white">
+      <h2 class="mt-2 text-xl leading-7 font-semibold text-gray-200 dark:text-white">
         {{ article.title }}
-      </h3>
-      <p class="mt-3 text-base leading-6 text-gray-100 dark:text-gray-400">
-        {{ article.description }}
+      </h2>
+      <p class="text-sm font-thin">
+        {{ articleDate }}
       </p>
+      <h3 class="mt-3 text-base leading-6 text-gray-100 dark:text-gray-400">
+        {{ article.description }}
+      </h3>
     </nuxt-link>
   </article>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 export default {
   props: {
     article: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    articleDate () {
+      return dayjs(this.article.createdAt).format('DD/MM/YYYY')
     }
   }
 }
