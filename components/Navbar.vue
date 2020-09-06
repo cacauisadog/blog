d82d7d8fee37804119ab9259740d2bc92b2c983d
cacauisadog <cbcaue@gmail.com>
2020-09-06 19:32:56 -0300

    adjusts the navbar layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,38 +1,26 @@
 <template>
-  <header class="w-screen bg-black fixed top-0 z-50">
-    <ul class="h-12 container max-w-screen-md px-4 md:px-0 flex text-lg text-gray-500 items-center">
+  <nav class="w-screen bg-black fixed top-0 z-50">
+    <ul class="container flex items-center max-w-screen-lg px-4 h-12 text-lg text-gray-500">
       <li class="cursor-pointer hover:text-gray-500">
-        <nuxt-link
-          :to="`/`"
-          class="p-2"
-        >
+        <nuxt-link :to="`/`">
           logo
         </nuxt-link>
       </li>
       <li class="ml-auto cursor-pointer hover:text-white">
-        <nuxt-link
-          :to="`/blog`"
-          class="p-2"
-        >
+        <nuxt-link :to="`/blog`">
           blog
         </nuxt-link>
       </li>
-      <li class="ml-2 md:ml-4 cursor-pointer hover:text-white">
-        <nuxt-link
-          :to="`/drops`"
-          class="p-2"
-        >
+      <li class="ml-8 md:ml-16 cursor-pointer hover:text-white">
+        <nuxt-link :to="`/drops`">
           drops
         </nuxt-link>
       </li>
-      <li class="ml-2 md:ml-4 cursor-pointer hover:text-white">
-        <nuxt-link
-          :to="`/about`"
-          class="p-2"
-        >
+      <li class="ml-8 md:ml-16 cursor-pointer hover:text-white">
+        <nuxt-link :to="`/about`">
           sobre
         </nuxt-link>
       </li>
     </ul>
-  </header>
+  </nav>
 </template>
