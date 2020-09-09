ed8a98f9338599cb962fbfb1fd0344694dbc965b
cacauisadog <cbcaue@gmail.com>
2020-09-08 22:44:28 -0300

    remove meta-viewport attrs that harm acessibility

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' },
+      { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0' },
       { hid: 'description', name: 'description', content: SEOData.description }
     ],
     link: [
